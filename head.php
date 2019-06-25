094216a4dcd2d8f8664f5f4d66c2b682dae764bb
Will Houston <wandrewhouston@gmail.com>
2019-06-25 14:32:48 -0400

    google ads test 1

--- head.php
+++ head.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-2277548402486690",
+        enable_page_level_ads: true
+    });
+    </script>
     <title>Parker's Podcast</title>
 </head>
 <body>
