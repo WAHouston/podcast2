8266156524f8a7668461dd1239afdc47629e6ed5
Will Houston <wandrewhouston@gmail.com>
2019-06-25 15:17:37 -0400

    copyright

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
 <footer>
-    <a id="" href=""><img class="social-link" src="assets/images/facebooklogo.png" alt="Parker's Podcast Facebook Page" /></a>
-    <a id="" href=""><img class="social-link" src="assets/images/instagramlogo.png" alt="Parker's Podcast Facebook Page" /></a>
-    <a id="" href=""><img class="social-link" src="assets/images/twitterlogo.png" alt="Parker's Podcast Facebook Page" /></a>
-    <a id="" href=""><img class="social-link" src="assets/images/patreonlogo.jpg" alt="Parker's Podcast Facebook Page" /></a>
+    <div id='social'>
+        <a id="" href=""><img class="social-link" src="assets/images/facebooklogo.png" alt="Parker's Podcast Facebook Page" /></a>
+        <a id="" href=""><img class="social-link" src="assets/images/instagramlogo.png" alt="Parker's Podcast Facebook Page" /></a>
+        <a id="" href=""><img class="social-link" src="assets/images/twitterlogo.png" alt="Parker's Podcast Facebook Page" /></a>
+        <a id="" href=""><img class="social-link" src="assets/images/patreonlogo.jpg" alt="Parker's Podcast Facebook Page" /></a>
+    <div>
+    <p id='copyright'>&copy;Copyright 2019 William A Houston</p>
 </footer>
 <script
     src="https://code.jquery.com/jquery-3.3.1.min.js"
